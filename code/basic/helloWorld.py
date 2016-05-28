a14adefadf46cf1cd2eef49e3086a1cee37a8a15
haiyang <zhy20090912@gmail.com>
2016-05-29 00:15:00 +0800

    add python tutorial code

---
+++ code/basic/helloWorld.py
@@ -0,0 +1,5 @@
+print('hell world', "my first", "python code")
+print(111 + 123)
+print("please input your name")
+name = input()
+print(name + " is a good boy")
---
+++ code/basic/synax.py
@@ -0,0 +1,42 @@
+'''
+print("Please input a number")
+a = int(input())
+if not a >= 0:
+    print(a)
+else:
+    print(-a)
+'''
+
+'''
+if not False: print("#ok>1");
+if not (not True): print("#ok>2");
+if not (True and False): print("#ok>3");
+if True or False: print("#ok>4");
+'''
+'''
+PI = 3.141592653
+print(PI)
+'''
+'''
+print(ord('a'))
+'''
+'''
+name = ['jack', 'summer', 'ivy', 'weiguo']
+print(name)
+print(len(name))
+print(name[0])
+print(name[-3])
+name.append('haiyang')
+print(name)
+name.insert(1, 'haiyang')
+print(name)
+name.pop()
+print(name)
+name.pop(0)
+print(name)
+'''
+'''
+if 1>2:
+    print("ok")
+'''
+
